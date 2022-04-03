18da977b323df714bf89fa275a92469bb366f217
gri38 <onzeweb@zaclys.net>
2022-04-03 12:01:34 +0200

    A working commit on all projects

--- clone-for-dev.sh
+++ clone-for-dev.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-for project in joplin-terminal-xapi django-joplin-vieweb; do
+for project in joplin-terminal-xapi django-joplin-vieweb joplin-vieweb-nginx joplin-vieweb-certbot; do
   git clone git@github.com:joplin-vieweb/${project}.git
 done
